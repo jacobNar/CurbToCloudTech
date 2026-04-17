a7dc1f7b3b4ebe349fadb38a128e09732eebc624
jacobNar <jdnarayan56@gmail.com>
2026-04-16 23:08:20 -0500

    add google tracking

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -15,30 +15,34 @@ export default function App({ Component, pageProps }) {
           --font-body: ${inter.style.fontFamily}, sans-serif;
         }
       `}</style>
-      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX" />
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-XXXXXXXXXX');
-        `}
-      </Script>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-4FV8DCQZ4V" />
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4FV8DCQZ4V');
+            `}
+          </Script>
 
-      <Script id="facebook-pixel" strategy="lazyOnload">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', 'YOUR_PIXEL_ID');
-          fbq('track', 'PageView');
-        `}
-      </Script>
+          <Script id="facebook-pixel" strategy="lazyOnload">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', 'YOUR_PIXEL_ID');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        </>
+      )}
 
       <Layout>
         <Component {...pageProps} />
